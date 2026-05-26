81389520778c7113f09f3899db38810860725b4b
Balaji <balaji@swipepages.com>
2026-05-26 10:41:39 +0530

    feat: replace icon.svg with high-resolution PNG icon set in manifest and UI

--- dist/vendor/vite-client.js
+++ dist/vendor/vite-client.js
@@ -833,7 +833,7 @@ const socketHost = `${"localhost" || importMetaUrl.hostname}:${hmrPort || import
 const directSocketHost = "localhost:5173/";
 const base = "/" || "/";
 const hmrTimeout = 30000;
-const wsToken = "UMzErY0heJJF";
+const wsToken = "B9RVBe5eMDsV";
 const isBundleMode = false;
 const forwardConsole = {"enabled":false,"unhandledErrors":false,"logLevels":[]};
 const transport = normalizeModuleRunnerTransport((() => {
